62ca167e797de92ebadb80f4027070c6eebc085b
SoniaChnini <chnini.sonia@gmail.com>
2018-11-09 23:49:57 +0100

    ios + android : entry and label renderer

--- Shoes/Shoes.iOS/LabelCustomRenderer.cs
+++ Shoes/Shoes.iOS/LabelCustomRenderer.cs
@@ -4,11 +4,25 @@ using System.Linq;
 using System.Text;
 
 using Foundation;
+using Shoes;
+using Shoes.iOS;
 using UIKit;
-
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.iOS;
+[assembly: ExportRenderer(typeof(LabelCustom), typeof(LabelCustomRenderer))]
 namespace Shoes.iOS
 {
-    class LabelCustomRenderer
+    class LabelCustomRenderer:LabelRenderer
     {
+        protected override void OnElementChanged(ElementChangedEventArgs<Label> e)
+        {
+            base.OnElementChanged(e);
+            if (Control != null)
+            {
+               
+                this.Layer.CornerRadius = 100;
+               Control.BackgroundColor = UIColor.LightGray;
+            }
+        }
     }
 }
